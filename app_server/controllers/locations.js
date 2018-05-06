f1617d317a05cf83b9512ace69674e23254927a7
Thomas Kacymarek <mulenatic@gmail.com>
2018-05-06 08:41:34 +0000

    Added new homepage with basic layout.

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -13,3 +13,6 @@ module.exports.addReview = function(req, res) {
     res.render('index', { title: 'Add review' });
 };
 
+module.exports.homelist = function(req, res) {
+    res.render('locations-list', {title : 'Home' });
+}
