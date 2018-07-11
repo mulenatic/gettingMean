1cea9209079348345f8c5304e4bf6c47692c77f4
mulenatic <mulenatic@gmail.com>
2018-07-11 13:48:34 +0200

    Added apiOptions for development and prod in locations controller.

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,4 +1,10 @@
 var request = require('request');
+var apiOptions = {
+    server : "http://localhost:3000"
+};
+if (process.env.NODE_ENV === 'production') {
+    apiOptions.server = "https://gettingmeanv2.herokuapp.com";
+};
 
 /* Get home page */
 module.exports.homelist = function(req, res) {
