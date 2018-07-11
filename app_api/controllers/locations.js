59e6e0a5e65fbdda9c9e91d6978b2d030129afa9
mulenatic <mulenatic@gmail.com>
2018-07-11 13:02:14 +0200

    Added method to delete a location.

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -169,7 +169,25 @@ module.exports.locationsUpdateOne = function(req, res) {
 };
 
 module.exports.locationsDeleteOne = function(req, res) {
-    sendJsonResponse(res, 200, {"status": "success"});
+    var locationid = req.params.locationid;
+    if(locationid) {
+	Loc
+	    .findByIdAndRemove(locationid)
+	    .exec(
+		function(err, location) {
+		    if (err) {
+			sendJsonResponse(res, 404, err);
+			return;
+		    }
+		    sendJsonResponse(res, 204, null);
+		}
+	    );
+    } else {
+	sendJsonResponse(res, 404, {
+	    "message" : "No locationid"
+	});
+    }
+			
 };
 
 
