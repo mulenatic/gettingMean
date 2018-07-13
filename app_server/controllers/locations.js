2d0c566f7823dde6e54ba8e733b102ec5cfd12b3
mulenatic <mulenatic@gmail.com>
2018-07-13 15:04:26 +0200

    Validating in the application layer.

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -162,16 +162,22 @@ module.exports.doAddReview = function(req, res) {
 	method: "POST",
 	json: postdata
     };
-    request(
-	requestOptions,
-	function(err, response, body) {
-	    if (response.statusCode === 201) {
-		res.redirect("/location/" + locationid );
-	    } else if (response.statusCode === 400 && body.name && body.name === "ValidationError" ) {
-		res.redirect("/location/" +  locationid + '/review/new?err=val');
-	    } else {
-		_showError(req, res, response.statusCode);
+
+    if (!postdata.author || !postdata.rating || !postdata.reviewText ) {
+	res.redirect("/location/" +  locationid + '/review/new?err=val');
+    } else {
+	
+	request(
+	    requestOptions,
+	    function(err, response, body) {
+		if (response.statusCode === 201) {
+		    res.redirect("/location/" + locationid );
+		} else if (response.statusCode === 400 && body.name && body.name === "ValidationError" ) {
+		    res.redirect("/location/" +  locationid + '/review/new?err=val');
+		} else {
+		    _showError(req, res, response.statusCode);
+		}
 	    }
-	}
-    );
+	);
+    }
 };
