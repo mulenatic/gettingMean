d8af25529fab101ae6576b6a75c0ede1b86b1394
mulenatic <mulenatic@gmail.com>
2018-07-16 09:16:03 +0200

    Corrected minor typos.

--- public/Angular/loc8rApp.js
+++ public/Angular/loc8rApp.js
@@ -7,35 +7,35 @@ var locationListCtrl = function($scope) {
 	    address: 'Teststr. 1',
 	    rating: 3,
 	    facilities: ['Beer', 'Food' ],
-	    distance: '0.3042'
+	    distance: '0.3042',
 	    _id: '23423rwdfasdfas23324fsfasdfa'
 	},{
 	    name: 'Burger Queen 2',
 	    address: 'Teststr. 2',
 	    rating: 3,
 	    facilities: ['Beer', 'Food' ],
-	    distance: '0.4042'
+	    distance: '0.4042',
 	    _id: '23423rwdfasdfas23324fsfasdfb'
 	},{
 	    name: 'Burger Queen 3',
 	    address: 'Teststr. 3',
 	    rating: 3,
 	    facilities: ['Beer', 'Food' ],
-	    distance: '0.6042'
+	    distance: '0.6042',
 	    _id: '23423rwdfasdfas23324fsfasdfc'
 	},{
 	    name: 'Burger Queen 4 ',
 	    address: 'Teststr. 4',
 	    rating: 3,
 	    facilities: ['Beer', 'Food' ],
-	    distance: '0.1042'
+	    distance: '0.1042',
 	    _id: '23423rwdfasdfas23324fsfasdfd'
 	},{
 	    name: 'Burger Queen 5',
 	    address: 'Teststr. 5',
 	    rating: 3,
 	    facilities: ['Beer', 'Food' ],
-	    distance: '0.0042'
+	    distance: '0.0042',
 	    _id: '23423rwdfasdfas23324fsfasdfe'
 	}]
     };
