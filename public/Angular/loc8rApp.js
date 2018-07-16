a587f9234024b28d2a126f3c49993602bad9d3a1
mulenatic <mulenatic@gmail.com>
2018-07-16 08:47:30 +0200

    Added angular to the loc8rApp.

--- public/Angular/loc8rApp.js
+++ public/Angular/loc8rApp.js
@@ -1,1 +1,46 @@
 angular.module('loc8rApp', []);
+
+var locationListCtrl = function($scope) {
+    $scope.data = {
+	locations: [{
+	    name: 'Burger Queen',
+	    address: 'Teststr. 1',
+	    rating: 3,
+	    facilities: ['Beer', 'Food' ],
+	    distance: '0.3042'
+	    _id: '23423rwdfasdfas23324fsfasdfa'
+	},{
+	    name: 'Burger Queen 2',
+	    address: 'Teststr. 2',
+	    rating: 3,
+	    facilities: ['Beer', 'Food' ],
+	    distance: '0.4042'
+	    _id: '23423rwdfasdfas23324fsfasdfb'
+	},{
+	    name: 'Burger Queen 3',
+	    address: 'Teststr. 3',
+	    rating: 3,
+	    facilities: ['Beer', 'Food' ],
+	    distance: '0.6042'
+	    _id: '23423rwdfasdfas23324fsfasdfc'
+	},{
+	    name: 'Burger Queen 4 ',
+	    address: 'Teststr. 4',
+	    rating: 3,
+	    facilities: ['Beer', 'Food' ],
+	    distance: '0.1042'
+	    _id: '23423rwdfasdfas23324fsfasdfd'
+	},{
+	    name: 'Burger Queen 5',
+	    address: 'Teststr. 5',
+	    rating: 3,
+	    facilities: ['Beer', 'Food' ],
+	    distance: '0.0042'
+	    _id: '23423rwdfasdfas23324fsfasdfe'
+	}]
+    };
+}
+
+angular
+    .module('loc8rApp')
+    .controller('locationListCtrl', locationListCtrl);
