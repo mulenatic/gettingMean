28d9dfba45b29d3188063f44bd5ef3614384c080
mulenatic <mulenatic@gmail.com>
2018-07-16 09:25:45 +0200

    Added filter to format the distance string.

--- public/Angular/loc8rApp.js
+++ public/Angular/loc8rApp.js
@@ -41,6 +41,30 @@ var locationListCtrl = function($scope) {
     };
 }
 
+
+var _isNumeric = function(n) {
+    return !isNan(parseFloat(n)) && isFinite(n);
+};
+
+var formatDistance = function() {
+    return function(distance) {
+	var numDistance, unit;
+	if (distance && _isNumeric(distance)) {
+	    if (distance > 1) {
+		numDistance = parseFloat(distance).toFixed(1);
+		unit = 'km';
+	    } else {
+		numDistance = parseInt(distance * 1000, 10);
+		unit = 'm';
+	    }
+	    return numDistance + unit;
+	} else {
+	    return "?";
+	}
+    };
+};
+
 angular
     .module('loc8rApp')
     .controller('locationListCtrl', locationListCtrl);
+    .filter('formatDistance', formatDistance);
