ce6cca176b6b3d5a9220bc1da2a6b162c68a0a63
mulenatic <mulenatic@gmail.com>
2018-07-17 08:45:42 +0200

    Added a rest call and tried to use it asynchronously.

--- public/Angular/loc8rApp.js
+++ public/Angular/loc8rApp.js
@@ -1,48 +1,18 @@
 angular.module('loc8rApp', []);
 
-var loc8rData = function() {
-
-    return [{
-	name: 'Burger Queen',
-	address: 'Teststr. 1',
-	rating: 1,
-	facilities: ['Beer', 'Food' ],
-	distance: '0.3042',
-	_id: '23423rwdfasdfas23324fsfasdfa'
-    },{
-	name: 'Burger Queen 2',
-	address: 'Teststr. 2',
-	rating: 2,
-	facilities: ['Beer', 'Food' ],
-	distance: '0.4042',
-	_id: '23423rwdfasdfas23324fsfasdfb'
-    },{
-	name: 'Burger Queen 3',
-	address: 'Teststr. 3',
-	rating: 3,
-	facilities: ['Beer', 'Food' ],
-	distance: '0.6042',
-	_id: '23423rwdfasdfas23324fsfasdfc'
-    },{
-	name: 'Burger Queen 4 ',
-	address: 'Teststr. 4',
-	rating: 4,
-	facilities: ['Beer', 'Food' ],
-	distance: '0.1042',
-	_id: '23423rwdfasdfas23324fsfasdfd'
-    },{
-	name: 'Burger Queen 5',
-	address: 'Teststr. 5',
-	rating: 5,
-	facilities: ['Beer', 'Food' ],
-	distance: '0.0042',
-	_id: '23423rwdfasdfas23324fsfasdfe'
-    }];
+var loc8rData = function($http) {
+    return $http.get('/api/locations?lng=6.8131&lat=50.9168&maxDistance=20');
 }
 
 var locationListCtrl = function($scope, loc8rData) {
-    $scope.data = { locations: loc8rData };
-}
+    loc8rData
+	.then(function(data) {
+	    $scope.data = { locations: data };
+	})
+	.catch(function(e) {
+	    consoloe.log(e);
+	});
+};
 
 
 var _isNumeric = function(n) {
