f8ad97a37fbad7e7a26ab0e4a1aaa6d82b59dd4e
mulenatic <mulenatic@gmail.com>
2018-07-18 09:26:21 +0200

    Naechster TEst.

--- app_client/home/home.controller.js
+++ app_client/home/home.controller.js
@@ -1,6 +1,7 @@
 angular
     .module('loc8rApp')
-    .controller('homeCtrl', homeCtrl);
+    .controller('homeCtrl', homeCtrl)
+    .service('loc8rData', loc8rData);
 
 function homeCtrl ($scope, loc8rData) {
 
@@ -16,21 +17,21 @@ function homeCtrl ($scope, loc8rData) {
 
     vm.message = "Checking your location!";
 
-    // vm.getData = function(position) {
-    // 	var lat = position.coords.latitude,
-    // 	    lng = position.coords.longitude;
-    // 	vm.message = "Searching for nearby places";
-    // 	loc8rData
-    // 	    .locationByCoords(lat, lng)
-    // 	    .success(function(data) {
-    // 		vm.message = data.length > 0 ? "" : "No locations found";
-    // 		vm.data = { locations: data };
-    // 	    })
-    // 	    .error(function(e) {
-    // 		vm.message = "Sorry, something's gone wrong";
-    // 		consoloe.log(e);
-    // 	    });
-    // };
+    vm.getData = function(position) {
+    	var lat = position.coords.latitude,
+    	    lng = position.coords.longitude;
+    	vm.message = "Searching for nearby places";
+    	loc8rData
+    	    .locationByCoords(lat, lng)
+    	    .success(function(data) {
+    		vm.message = data.length > 0 ? "" : "No locations found";
+    		vm.data = { locations: data };
+    	    })
+    	    .error(function(e) {
+    		vm.message = "Sorry, something's gone wrong";
+    		consoloe.log(e);
+    	    });
+    };
 
     vm.showError = function(error) {
     	$scope.$apply(function() {
@@ -44,7 +45,18 @@ function homeCtrl ($scope, loc8rData) {
     	});
     };
 
-    // vm.getData({coords: { latitude: "50.9168", longitude: "6.8131" }});
+    vm.getData({coords: { latitude: "50.9168", longitude: "6.8131" }});
     // //geolocation.getPosition(vm.getData, vm.showError, vm.noGeo);
 
 }
+
+
+var loc8rData = function($http) {
+    var locationByCoords = function(lat, lng) {
+	return $http.get('/api/locations?lng=' + lng + '&lat=' + lat + '&maxDistance=20');
+    };
+
+    return {
+	locationByCoords: locationByCoords
+    };
+};
